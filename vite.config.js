8d57f5f5a5f72b281f3594ac1b845e73ed183228
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2024-07-03 15:44:13 -0700

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
-	plugins: [sveltekit()]
-  
+  plugins: [sveltekit()],
+  define: {
+    'process.env.VITE_SUPABASE_URL': JSON.stringify(process.env.VITE_SUPABASE_URL),
+    'process.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(process.env.VITE_SUPABASE_ANON_KEY),
+  }
 });
