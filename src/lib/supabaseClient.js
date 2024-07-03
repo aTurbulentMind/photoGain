92756dcd4be0f92ccf33485e2170dc2760071c88
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2024-07-03 14:05:56 -0700

    Add files via upload

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.supabaseUrl;
-const supabaseAnonKey = import.meta.env.supabaseAnonKey;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
