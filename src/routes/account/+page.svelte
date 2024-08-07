8151bec3ff0679d99b1e7adca281106d7b59a020
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2024-08-07 13:12:56 -0700

    Update +page.svelte

--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -88,13 +88,7 @@
 
 	<br />
 
-	<div class="window-content">
-		<form method="post" action="?/signout" use:enhance={handleSignOut}>
-			<div>
-				<button class="win_95_butt" disabled={loading}>Sign Out</button>
-			</div>
-		</form>
-	</div>
+
 </div>
 
 <div class="win_95">
